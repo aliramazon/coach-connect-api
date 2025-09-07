9e32f63b694a2231d408a566dff82bdf564a8654
Ali Ramazon <mr.safarali@gmail.com>
2025-09-06 18:18:54 -0600

    Write function to seed db with users

---
+++ src/prisma/seed/seed-users.ts
@@ -0,0 +1,55 @@
+import { faker } from '@faker-js/faker';
+import { config } from 'dotenv';
+import { UserRole } from '../../generated/prisma/enums';
+import { userService } from '../../services/user.service';
+config();
+
+const generateRandomUserInfo = (role: UserRole) => {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    return {
+        email: faker.internet.email({
+            firstName,
+            lastName,
+            provider: 'gmail.com',
+        }),
+        phoneNumber: faker.phone.number({
+            style: 'national',
+        }),
+        role: role,
+        firstName,
+        lastName,
+    };
+};
+
+const generateUsers = () => {
+    const users = [];
+
+    for (let i = 1; i <= 15; i++) {
+        users.push(
+            generateRandomUserInfo(i <= 10 ? UserRole.STUDENT : UserRole.COACH),
+        );
+    }
+
+    return users;
+};
+
+const seedUsers = async () => {
+    const users = generateUsers();
+
+    const data = await Promise.all(
+        users.map((user) => userService.create(user)),
+    );
+
+    await Promise.all(
+        data.map(({ createdUser, inviteToken }) =>
+            userService.createPassword(
+                inviteToken,
+                `${createdUser.firstName}${createdUser.lastName}`,
+                createdUser.email,
+            ),
+        ),
+    );
+};
+
+seedUsers();
