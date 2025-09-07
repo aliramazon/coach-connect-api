062c7f38ee68147afcf9b1af108ab657be8e7ced
Ali Ramazon <mr.safarali@gmail.com>
2025-09-06 23:01:38 -0600

    Verify CSRF token

---
+++ src/middlewares/verify-csrf.middleware.ts
@@ -0,0 +1,23 @@
+import { catchAsync } from '../utils/catch-async';
+import { CustomError } from '../utils/custom-error';
+
+export const verifyCSRF = catchAsync(async (req, _, next) => {
+    const csrfCookie = req.cookies.csrfToken;
+    const csrfHeader = req.headers['x-csrf-token'] as string;
+
+    if (!csrfCookie) {
+        throw CustomError.authenticationFailed(
+            'Session expired, please login again',
+        );
+    }
+
+    if (!csrfHeader) {
+        throw CustomError.forbidden('Invalid request');
+    }
+
+    if (csrfCookie !== csrfHeader) {
+        throw CustomError.forbidden('Invalid request');
+    }
+
+    next();
+});
